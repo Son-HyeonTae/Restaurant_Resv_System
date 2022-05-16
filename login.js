9f41f20ef4900d6f90331e807b0e0f8d1ffda47d
여운용 <49231362+uny00@users.noreply.github.com>
2022-05-16 18:31:46 +0900

    페이지 연결 수정
    페이지 연결 수정

--- login.js
+++ login.js
@@ -4,11 +4,9 @@ function validate(){
 
     if(id=="user"&&password=="1234"){
         alert("회원님 환영합니다.");
-        location.href = index_after.html;
     }
     else if(id=="admin"&&password=="123456"){
         alert("관리자님 환영합니다.");
-        location.href = admin.html;
     }
     else{
         alert("입력하신 정보가 맞지 않습니다.");
