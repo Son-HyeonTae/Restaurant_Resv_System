e270517e2439e197eb84e6f1cc5de2a2a2688d84
여운용 <49231362+uny00@users.noreply.github.com>
2022-05-16 14:31:58 +0900

    로그인 체크 기능
    회원 로그인 성공, 실패, 관리자 확인 기능

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
-# Team-E Repository
+# SW-Project ( Front End )
 
-## Frontend
-Restaurant Reservation Page System ( Team E )
+login 했을 때
+회원, 관리자, 실패시 각각에 맞는 이벤트 출력
+그리고 로그인 후, 페이지 연결 코드까지 넣었습니다.
 
-VS Code
+logout은 박건님이 logout 버튼 누르면 index로 가게 하신거 확인했습니다.
---
+++ login.js
@@ -0,0 +1,17 @@
+function validate(){
+    var id=document.getElementById("id").value;
+    var password=document.getElementById("password").value;
+
+    if(id=="user"&&password=="1234"){
+        alert("회원님 환영합니다.");
+        location.href = index_after.html;
+    }
+    else if(id=="admin"&&password=="123456"){
+        alert("관리자님 환영합니다.");
+        location.href = admin.html;
+    }
+    else{
+        alert("입력하신 정보가 맞지 않습니다.");
+    }
+}
+
